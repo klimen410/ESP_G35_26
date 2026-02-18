6b2ac96330f2f13d1fbd13cc95c9c6e6ee57340d
klimen410 <clementius.the@student.manchester.ac.uk>
2026-02-18 11:52:50 +0000

    Change1

--- PWM.cpp
+++ PWM.cpp
@@ -1,8 +1,79 @@
 #include "mbed.h"
 
-class PWM{
-    private:
-    PwmOut PWML, PWMR;
+#define PWM_FREQUENCY 1000.0f
+#define DEFAULT_PWM 0.5f
 
-    public:
+PwmOut PWM_LEFT(PA_5);
+PwmOut PWM_RIGHT(PA_6);
+
+DigitalOut MDBEnable(D7);
+DigitalOut BPE1(D8);
+DigitalOut BPE2(D9);
+class Potentiometer                                                     //Begin updated potentiometer class definition
+{
+private:                                                                //Private data member declaration
+    AnalogIn inputSignal;                                               //Declaration of AnalogIn object
+    float VDD, currentSampleNorm, currentSampleVolts;                   //Float variable to speficy the value of VDD (3.3 V for the Nucleo-64)
+
+public:                                                                 // Public declarations
+    Potentiometer(PinName pin, float v) : inputSignal(pin), VDD(v) {}   //Constructor - user provided pin name assigned to AnalogIn...
+                                                                        //VDD is also provided to determine maximum measurable voltage
+    float amplitudeVolts(void)                                          //Public member function to measure the amplitude in volts
+    {
+        return (inputSignal.read()*VDD);                                //Scales the 0.0-1.0 value by VDD to read the input in volts
+    }
+    float amplitudeNorm(void)                                           //Public member function to measure the normalised amplitude
+    {
+        return inputSignal.read();                                      //Returns the ADC value normalised to range 0.0 - 1.0
+    }
+    void sample(void)                                                   //Public member function to read a sample and store the value as data members
+    {
+        currentSampleNorm = inputSignal.read();                         //Read a sample from the ADC and store normalised representation [0..1]
+        currentSampleVolts = currentSampleNorm*VDD;                     //Convert this to a voltage and store that as a data member too.
+    }
+     float getCurrentSampleNorm(void)                              //Public member function to return the most recent normalised sample [0..1]
+    {
+        return currentSampleNorm;                                       //Return the most recent normalised sample
+    }
+     float getCurrentSampleVolts(void)                             //Public member function to return the most recent sampled voltage [0.. 3.3 V]
+    {
+        return currentSampleVolts;                                      //Return the most recent sampled voltage
+    }
 };
+class SamplingPotentiometer : public Potentiometer{
+    private:
+    float samplingFrequency, samplingPeriod;
+    Ticker sampler;
+    void periodicSample() {
+        Potentiometer::sample();   // Call base class sample()
+    }
+    public:
+    SamplingPotentiometer(PinName p, float v, float fs): Potentiometer(p,v), samplingFrequency(fs){
+        samplingPeriod = 1.0f/samplingFrequency;
+        sampler.attach(callback(this, &SamplingPotentiometer::periodicSample), samplingPeriod);
+        }
+};
+void setPWMDuty(float leftPWM, float rightPWM) {
+    if (leftPWM > 1.0f)  leftPWM = 1.0f;
+    if (leftPWM < 0.0f)  leftPWM = 0.0f;
+    if (rightPWM > 1.0f) rightPWM = 1.0f;
+    if (rightPWM < 0.0f) rightPWM = 0.0f;
+
+    PWM_LEFT.write(leftPWM);
+    PWM_RIGHT.write(rightPWM);
+}
+
+int main() {
+    SamplingPotentiometer potX(A0, 3.3f, 100.0f);
+    PWM_LEFT.period(1.0f / PWM_FREQUENCY);
+    PWM_RIGHT.period(1.0f / PWM_FREQUENCY);
+    setPWMDuty(0.5f, 0.5f);
+    MDBEnable.write(1); 
+    BPE1.write(1);
+    BPE2.write(1);
+    while (true) {
+        float potValue = potX.getCurrentSampleNorm();
+        setPWMDuty(potValue, potValue);
+        wait_ms(20); 
+    }
+}
