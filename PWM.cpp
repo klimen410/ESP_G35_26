164cca0b2e35cebacc08f1ad63d27581f16e9c45
klimen410 <clementius.the@student.manchester.ac.uk>
2026-02-20 17:32:17 +0000

    Encoder&PWM

--- PWM.cpp
+++ PWM.cpp
@@ -103,7 +103,7 @@ int main() {
         encoderLeft_prev = encoderLeft_current;
         encoderRight_prev = encoderRight_current;
 
-        wait_ms(30); //sample tim
+        wait_ms(30); //sample time
 
         time_curr = time.read();
         sampletime = time_curr - time_prev;
