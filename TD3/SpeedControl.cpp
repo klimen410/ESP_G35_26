cde2e1463a0574529fa80ffd17b0c2347639f897
klimen410 <clementius.the@student.manchester.ac.uk>
2026-04-13 16:38:38 +0000

    test

--- TD3/SpeedControl.cpp
+++ TD3/SpeedControl.cpp
@@ -27,15 +27,12 @@ void stopMotors() {
 //This code use for in slope or resistance increase, the speed don't change
 class SpeedController {
 private:
-    float Kp;               // Proportional gain
-    float Ki;               // Integral gain
+    float Kp, Ki;               // Proportional gain
     float targetSpeed;      // Desired setpoint (RPM)
     float integralError;    // Accumulated error over time (Integral term)
     float currentPWM;       // Current calculated PWM duty cycle (0.0 to 1.0)
-    Encoder &ENCODER_LEFT;
-    Timer t;
 public:
-    SpeedController(float p, float i, Encoder &enc) : Kp(p), Ki(i), ENCODER_LEFT(enc),targetSpeed(0), integralError(0), currentPWM(0.5f) {}
+    SpeedController(float p, float i) : Kp(p), Ki(i), targetSpeed(0), integralError(0) {}
     //Set the desired wheel speed
     void setTargetSpeed(float rpm) {
         targetSpeed = rpm;
@@ -45,15 +42,18 @@ public:
     float update(float measuredRPM, float dt) {
         // Calculate the error between setpoint and feedback
         float error = targetSpeed - measuredRPM;
-        
         // Update the integral term (error accumulation)
         integralError += error * dt;
-
+        if (integralError > 10.0f) integralError = 10.0f;
+        if (integralError < -10.0f) integralError = -10.0f;
         // PI Calculation
         float output = (Kp * error) + (Ki * integralError);
         
         // Offset the PI output from the neutral/stop point (0.5 for bipolar drive)
         currentPWM = 0.5f + output;
+        if (currentPWM > 1.0f) currentPWM = 1.0f;
+        if (currentPWM < 0.0f) currentPWM = 0.0f;
+
         return currentPWM;
     }
 };
@@ -69,8 +69,12 @@ int main() {
     Encoder ENCODER_LEFT(PC_10, PC_12);
     Encoder ENCODER_RIGHT(PB_13, PB_14);
 
-    SpeedController SpeedControlLeft(1,1,ENCODER_LEFT);
-    SpeedController SpeedControlRight(1,1,ENCODER_RIGHT);
+    SpeedController SpeedControlLeft(0.006f, 0.0015f);
+    SpeedController SpeedControlRight(0.006f, 0.0015f);
+    Timer t;
+    int prevLeft = 0;
+    int prevRight = 0;
+
     ENCODER_LEFT.reset();
     ENCODER_RIGHT.reset();
     SpeedControl.setTargetSpeed(float rpm);
@@ -84,11 +88,21 @@ int main() {
         float right_tickrate = (float)(currRight - prevRight) / sampleTime;
         float left_RPM = (left_tickrate * 60.0f) / (float)COUNTS_PER_REV;
         float right_RPM = (right_tickrate * 60.0f) / (float)COUNTS_PER_REV;
-        float left_PWM = SpeedControlLeft.update(left_RPM, 0.05);
-        float right_PWM = SpeedControlRight.update(right_RPM, 0.05);
-        MDBEnable = 1;
+        prevLeft = currLeft;
+        prevRight = currRight;
+        float left_PWM = SpeedControlLeft.update(left_RPM, SAMPLE_TIME);
+        float right_PWM = SpeedControlRight.update(right_RPM, SAMPLE_TIME);
+
         PWM_LEFT.write(left_PWM); 
         PWM_RIGHT.write(right_PWM); 
+        LCD.cls();
+        LCD.locate(0,0);
+        LCD.printf("Target: %.1f RPM", TARGET_RPM);
+        LCD.locate(0,10);
+        LCD.printf("L:%.1f R:%.1f", left_RPM, right_RPM);
+        LCD.locate(0,20);
+        LCD.printf("LPWM:%.2f RPWM:%.2f", left_PWM, right_PWM)
+        while (t.read() < SAMPLE_TIME);
     }
 
 }
